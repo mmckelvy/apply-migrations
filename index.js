f849e82570797d8081abbe2b0e2ca3454511f4f9
Mark McKelvy <mmckelvy@gmail.com>
2020-09-26 15:42:50 -0700

    better error handling and tests

--- index.js
+++ index.js
@@ -10,50 +10,89 @@ module.exports = async function applyMigrations({ pool, src }) {
   let appliedMigrations;
 
   try {
-    const results = await query({
-      pool,
-      sql: `${__dirname}/get-migrations.sql`
-    });
+    /*
+    Capture any applied migrations.
+    If this is the first time this script is run
+    there won't be a migrations table so this will throw an error
+    that we'll need to catch.
+    */
+    try {
+      const results = await query({
+        pool,
+        sql: `${__dirname}/get-migrations.sql`
+      });
 
-    appliedMigrations = results.rows;
+      appliedMigrations = results.rows;
 
-    console.log('Migrations already applied:\n');
-    console.log(appliedMigrations);
+      console.log('Migrations already applied:\n');
+      console.log(appliedMigrations);
 
-  } catch (err) {
-    appliedMigrations = [];
-  }
+    } catch (err) {
+      appliedMigrations = [];
+    }
 
-  const pendingMigrations = diffArrays(
-    allMigrations,
-    appliedMigrations.map(m => m.filename)
-  );
+    /*
+    Set up and sort any pending migrations.
+    */
+    const pendingMigrations = diffArrays(
+      allMigrations,
+      appliedMigrations.map(m => m.filename)
+    );
 
-  versionSort(pendingMigrations, {inPlace: true});
+    versionSort(pendingMigrations, {inPlace: true});
 
-  if (!pendingMigrations.length) {
-    console.log('No migrations to apply.');
+    // No migrations, bail.
+    if (!pendingMigrations.length) {
+      console.log('No migrations to apply.');
 
-    await pool.end();
-    return;
-  }
+      await pool.end();
+      console.log('Ended the migration pool.');
+      return;
+    }
 
-  for (const migration of pendingMigrations) {
-    // apply the migration
-    await query({
-      pool,
-      sql: `${src}/${migration}`
-    });
+    /*
+    Apply any pending migrations.
+    */
+    for (const migration of pendingMigrations) {
+      // We want to log any migration failure, so we'll
+      // wrap each attempt in a try/catch
+      try {
+        // apply the migration
+        await query({
+          pool,
+          sql: `${src}/${migration}`
+        });
 
-    // update the migration table in the db
-    await query({
-      pool,
-      sql: `${__dirname}/record-migration.sql`,
-      values: {
-        filename: migration
+        // update the migration table in the db
+        await query({
+          pool,
+          sql: `${__dirname}/record-migration.sql`,
+          values: {
+            filename: migration
+          }
+        });
+
+        console.log(`${migration} applied.`);
+
+      } catch (err) {
+        console.log(`${migration} FAILED.`);
+        // pass control to the main error handler.
+        throw err;
       }
-    });
 
-    console.log(`Migration ${migration} applied.`);
+      /*
+      Success. End the pool and log the results.
+      */
+
+      await pool.end();
+      console.log('Ended the migration pool.');
+    }
+
+  // Main error handler
+  } catch (err) {
+    console.log('There was a problem with the migrations:\n');
+    console.log(err);
+    await pool.end();
+    console.log('Ended the migration pool.');
   }
 };
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
+const { Pool } = require('pg');
+
+const test = require('ava');
 const { exec } = require('child_process');
 const { promisify } = require('util');
 const execAsync = promisify(exec);
-
-const { Pool } = require('pg');
-const test = require('ava');
 
 const applyMigrations = require('../index');
 
@@ -13,8 +13,18 @@ test.before(async (t) => {
   try {
     // Spin up a db and the pool
     await execAsync(`createdb ${db}`);
+    console.log(`Database ${db} created.`);
 
-    t.context.pool = new Pool({
+    t.context.migrationsPool = new Pool({
+      host: 'localhost',
+      database: db,
+      port: 5432,
+      user: process.env.USER,
+      password: null,
+      max: 10
+    });
+
+    t.context.testPool = new Pool({
       host: 'localhost',
       database: db,
       port: 5432,
@@ -25,18 +35,19 @@ test.before(async (t) => {
 
   } catch (err) {
     console.log(err);
-    console.log('Could not create db migrations-test');
+    console.log('Failed at setup.');
   }
 });
 
-test('From a blank slate', async (t) => {
+// Simple base case.
+test('applyMigrations - Case 1', async (t) => {
   await applyMigrations({
-    pool: t.context.pool,
+    pool: t.context.migrationsPool,
     src: `${__dirname}/src`
   });
 
   // verify that base-schema was recorded
-  const results = await t.context.pool.query({
+  const results = await t.context.testPool.query({
     text: `select filename from migration`
   });
 
@@ -44,7 +55,9 @@ test('From a blank slate', async (t) => {
 });
 
 test.after.always('Clean up the db', async (t) => {
-  await t.context.pool.end();
+  await t.context.testPool.end();
+  console.log('Ended the test pool.');
+
   await execAsync(`dropdb ${db}`);
-  console.log(`${db} dropped.`);
+  console.log(`Database ${db} dropped.`);
 });
